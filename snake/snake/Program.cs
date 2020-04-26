244e8507dac08c4b56f7f401f5ec1bf575469139
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-04-26 13:48:45 +0600

    new point

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,15 +10,15 @@ namespace snake
         static void Main(string[] args)
         {
             Point point1 = new Point();
-            point1.x = 1;
-            point1.y = 2;
+            point1.x = 10;
+            point1.y = 20;
             point1.sym = '*';
             point1.Draw();
 
 
             Point point2 = new Point();
-            point2.x = 3;
-            point2.y = 4;
+            point2.x = 30;
+            point2.y = 40;
             point2.sym = '#';
             point2.Draw();
 
